1a9778aae59b227548e5909662975b044f113458
Andy Hang <hang.andy.t@gmail.com>
2018-08-08 15:52:19 -0500

    added association between user and articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.firsti
 
     if @article.save
       flash[:success] = 'Ze Article was Created'
