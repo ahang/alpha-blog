7e7605aac16512fb1f07684c843caccee9237925
Andy Hang <hang.andy.t@gmail.com>
2018-08-09 14:50:35 -0500

    added admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = 'You cannot edit or delete another User Article'
         redirect_to root_path
       end
